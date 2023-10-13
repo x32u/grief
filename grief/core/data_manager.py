c7ce2847154add15b9a852f4b9db702401313aa8
resent <contact@resent.dev>
2023-10-13 01:26:18 -0400

    p

--- grief/core/data_manager.py
+++ grief/core/data_manager.py
@@ -186,43 +186,6 @@ def core_data_path() -> Path:
 
     return core_path.resolve()
 
-
-def bundled_data_path(cog_instance: commands.Cog) -> Path:
-    """
-    Get the path to the "data" directory bundled with this cog.
-
-    The bundled data folder must be located alongside the ``.py`` file
-    which contains the cog class.
-
-    .. important::
-
-        You should *NEVER* write to this directory.
-
-    Parameters
-    ----------
-    cog_instance
-        An instance of your cog. If calling from a command or method of
-        your cog, this should be ``self``.
-
-    Returns
-    -------
-    pathlib.Path
-        Path object to the bundled data folder.
-
-    Raises
-    ------
-    FileNotFoundError
-        If no bundled data folder exists.
-
-    """
-    bundled_path = Path(inspect.getfile(cog_instance.__class__)).parent / "data"
-
-    if not bundled_path.is_dir():
-        raise FileNotFoundError("No such directory {}".format(bundled_path))
-
-    return bundled_path
-
-
 def data_path() -> Path:
     """Gets the base data path.
 
