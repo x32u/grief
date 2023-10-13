1f8174706b66cc2d16971dd20268547fc3af3cc9
resent <contact@resent.dev>
2023-10-13 11:53:58 -0400

    p

--- grief/core/utils/views.py
+++ grief/core/utils/views.py
@@ -4,7 +4,7 @@ import discord
 
 from discord.ext.commands import BadArgument
 from typing import TYPE_CHECKING, Any, List, Optional, Union, Dict
-from grief.i18n import Translator
+from grief.core.i18n import Translator
 from redbot.vendored.discord.ext import menus
 from grief.commands.converter import get_dict_converter
 
