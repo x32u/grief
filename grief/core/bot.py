21964be17551436263e7f6aca64e19208ca1169d
resent <contact@resent.dev>
2023-10-13 09:42:49 -0400

    p

--- grief/core/bot.py
+++ grief/core/bot.py
@@ -37,7 +37,7 @@ import discord
 from discord.ext import commands as dpy_commands
 from discord.ext.commands import when_mentioned_or
 
-from . import Config, i18n, app_commands, commands, errors, _drivers, modlog, bank
+from . import Config, i18n, app_commands, commands, errors, _drivers
 from ._cli import ExitCodes
 from ._cog_manager import CogManager, CogManagerUI
 from .core_commands import Core
