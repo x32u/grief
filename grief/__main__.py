8e460c1b3478b495920a63e96abfd502a9c765ed
resent <contact@resent.dev>
2023-10-13 01:41:18 -0400

    p

--- grief/__main__.py
+++ grief/__main__.py
@@ -306,7 +306,7 @@ async def run_bot(red: Red, cli_flags: Namespace) -> None:
 
     await driver_cls.initialize(**data_manager.storage_details())
 
-    redbot.logging.init_logging(
+    grief.logging.init_logging(
         level=cli_flags.logging_level,
         location=data_manager.core_data_path() / "logs",
         cli_flags=cli_flags,
