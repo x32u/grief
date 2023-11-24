3c02300efedacd01d6410a5818a971c55292bb00
resent <contact@resent.dev>
2023-11-24 16:44:09 -0500

    p

--- grief/__main__.py
+++ grief/__main__.py
@@ -490,7 +490,7 @@ def main():
 
         data_manager.load_basic_configuration(cli_flags.instance_name)
 
-        grief = grief(cli_flags=cli_flags, description="grief V3", dm_help=None)
+        grief = grief(parse_cli_flags=cli_flags, description="grief V3", dm_help=None)
 
         if os.name != "nt":
             # None of this works on windows.
